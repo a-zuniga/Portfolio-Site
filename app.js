68b10b1344cebd490e2da3d2f195dc550dbc44c8
Alejandro Zuniga <azunigas@gmu.edu>
2020-08-05 22:56:13 -0400

    Routed express app to home and 404

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Home route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile('home.html', {root : __dirname + '/assets'});
+})
+
+// 404 Response
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`listening at http://localhost:${port}`)
 })
