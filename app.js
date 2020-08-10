f98ba96c92c9a8d423bc309a27841f1d2e69a24f
Alejandro Zuniga <azunigas@gmu.edu>
2020-08-09 21:57:52 -0400

    changing port to environment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 // Lets express load the files that are in assets
 app.use(express.static(__dirname + '/assets'));
@@ -16,5 +16,5 @@ app.use(function (req, res, next) {
 })
 
 app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`)
+  console.log(`listening at http://localhost:${PORT}`)
 })
